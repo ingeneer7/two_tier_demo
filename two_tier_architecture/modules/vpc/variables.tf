5b073e9f4835ab2a6327ae7615036ea45110c3b5
ingeneer7 <52728909+ingeneer7@users.noreply.github.com>
2023-03-11 17:04:28 -0500

    Create variables.tf

---
+++ two_tier_architecture/modules/vpc/variables.tf
@@ -0,0 +1,8 @@
+variable "regiom" {}
+variable "vpc_cidr" {}
+variable "public_az1_cidr" {}
+variable "public_az2_cidr" {}
+variable "private_az1_cidr" {}
+variable "private_az2_cidr" {}
+variable "private_data_az1_cidr" {}
+variable "private_data_az2_cidr" {}
